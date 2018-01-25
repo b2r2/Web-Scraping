78278cbca3d0b7386b0aaadf3b2ea673bb8ac9ff
b2r2 <b2r2@bk.ru>
2018-01-25 20:18:37 +0300

    Edited function parse_text_datafile

--- web_scraping.py
+++ web_scraping.py
@@ -6,7 +6,7 @@ import re
 from bs4 import BeautifulSoup
 
 
-article = 'Ispolzovanie-regulyarnyh-vyrazhenij-v-Python-dlya-novichkov-08-04'
+url = 'telegra.ph/Ispolzovanie-regulyarnyh-vyrazhenij-v-Python-dlya-novichkov-08-04'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/58.0.3029.81 Chrome/58.0.3029.81 Safari/537.36'
@@ -15,25 +15,23 @@ s = requests.Session()
 s.headers.update(headers)
 
 
-def load_content(article, session):
-    url = 'https://telegra.ph/{}'.format(article)
+def load_content(url, session):
+    url = 'https://{}'.format(url)
     request = session.get(url)
     return request.text
 
 
 def parse_text_datafile(content):
     soup = BeautifulSoup(content, 'lxml')
-    content_text = soup.find_all('div', {'class': 'tl_page'})
-
-    return content_text
+    return soup.__str__()
 
 
 def to_count_rus_letters(text):
-    return len(''.join(re.findall('[А-Яа-я]+', text.decode('utf-8'))))
+    return len(''.join(re.findall('[А-Яа-я]+', text)))
 
 
 def main():
-    data = load_content(article, s)
+    data = load_content(url, s)
     content = parse_text_datafile(data)
     print(to_count_rus_letters(content))
 
