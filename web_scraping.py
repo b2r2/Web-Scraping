ca923792a2f458979d94017f5ef2b2d0adb73e95
b2r2 <b2r2@bk.ru>
2018-01-28 23:14:44 +0300

    Refactoring: rewrote code on the class

--- web_scraping.py
+++ web_scraping.py
@@ -6,34 +6,32 @@ import re
 from bs4 import BeautifulSoup
 
 
-url = 'telegra.ph/Nachnem-rabotu-s-Mock-v-Python-01-26'
+class WebScraping:
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/58.0.3029.81 Chrome/58.0.3029.81 Safari/537.36'
+        }
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/58.0.3029.81 Chrome/58.0.3029.81 Safari/537.36'
-}
-s = requests.Session()
-s.headers.update(headers)
+    def load_content(self, url):
+        request = self.session.get(url)
+        return request.text
 
+    def parse_text_data(self, content):
+        soup = BeautifulSoup(content, 'lxml')
+        return soup.find('article', {'class': 'tl_article_content'}).text
 
-def load_content(url, session):
-    url = 'https://{}'.format(url)
-    request = session.get(url)
-    return request.text
+    def to_count_rus_letters(self, text):
+        return len(''.join(re.findall('[А-Яа-я]+', text)))
 
-
-def parse_text_data(content):
-    soup = BeautifulSoup(content, 'lxml')
-    return soup.find('article', {'class': 'tl_article_content'}).text
-
-
-def to_count_rus_letters(text):
-    return len(''.join(re.findall('[А-Яа-я]+', text)))
+    def input_data(self):
+        pass
 
 
 def main():
-    data = load_content(url, s)
-    content = parse_text_data(data)
-    print(to_count_rus_letters(content))
+    scaping = WebScraping()
+    scaping.run()
 
 
 if __name__ == '__main__':
