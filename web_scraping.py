e29b2e4fe24a265dc4de56a25c650561cdaa418d
b2r2 <b2r2@bk.ru>
2018-02-01 11:36:16 +0300

    Refactoring: added 2 function and mainloop

--- web_scraping.py
+++ web_scraping.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
+import sys
 import requests
 import re
+import gui
 from bs4 import BeautifulSoup
 
 
@@ -25,3 +27,27 @@ class Scraper():
     def get_text_size(self, text):
         rus_letters = re.findall('[А-Яа-я]+', text)
         return len(''.join(rus_letters))
+
+
+def on_click(url, QMessageBox):
+    size_text = get_size_text(url)
+    output_text = 'All Russian symbols\n'
+    message_box = QMessageBox()
+    message_box.setText(output_text + str(size_text))
+    message_box.setWindowTitle('Counter')
+    message_box.setIcon(QMessageBox.Information)
+    message_box.setStandardButtons(QMessageBox.Ok)
+    message_box.exec_()
+
+
+def get_size_text(url):
+    scraper = Scraper()
+    data = scraper.load_content(url)
+    content = scraper.parse_text_data(data)
+    return scraper.get_text_size(content)
+
+
+if __name__ == '__main__':
+    app = gui.QApplication(sys.argv)
+    window = gui.MainWindow()
+    sys.exit(app.exec_())
