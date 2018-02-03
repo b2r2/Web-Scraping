68983ea361dbfe9507b3a26ed49e889107cf9dd8
b2r2 <b2r2@bk.ru>
2018-02-03 23:33:31 +0300

    Refactoring: deleted two methods and created method "run"

--- web_scraping.py
+++ web_scraping.py
@@ -2,6 +2,7 @@
 
 
 import sys
+import os
 import requests
 import re
 import gui
@@ -16,35 +17,22 @@ class Scraper():
         self.session = requests.Session()
         self.session.headers.update(self.headers)
 
-    def load_content(self, url):
+    def __load_content(self, url):
         request = self.session.get(url)
         return request.text
 
-    def parse_text_data(self, content):
+    def __parse_text_data(self, content):
         soup = BeautifulSoup(content, 'lxml')
         return soup.find('article', {'class': 'tl_article_content'}).text
 
-    def get_text_size(self, text):
+    def __get_text_size(self, text):
         rus_letters = re.findall('[А-Яа-я]+', text)
         return len(''.join(rus_letters))
 
-
-def on_click(url, QMessageBox):
-    size_text = get_size_text(url)
-    output_text = 'All Russian symbols\n'
-    message_box = QMessageBox()
-    message_box.setText(output_text + str(size_text))
-    message_box.setWindowTitle('Counter')
-    message_box.setIcon(QMessageBox.Information)
-    message_box.setStandardButtons(QMessageBox.Ok)
-    message_box.exec_()
-
-
-def get_size_text(url):
-    scraper = Scraper()
-    data = scraper.load_content(url)
-    content = scraper.parse_text_data(data)
-    return scraper.get_text_size(content)
+    def run(self, url):
+        data = self.__load_content(url)
+        content = self.__parse_text_data(data)
+        return self.__get_text_size(content)
 
 
 if __name__ == '__main__':
